08ed0390da9587c48a918d200e116ab90f198f6e
Liyze09 <a52990419@outlook.com>
2025-10-17 23:41:31 +0800

    Initial Comment

---
+++ src/client/java/io/github/liyze09/nexus/NexusClientMain.java
@@ -0,0 +1,10 @@
+package io.github.liyze09.nexus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NexusClientMain implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
